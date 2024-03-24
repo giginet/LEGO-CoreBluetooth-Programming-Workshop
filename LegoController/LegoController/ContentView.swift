452986b34b9257b51f6c15e59da64b37965dbe34
Thea36 <73578961+Thea36@users.noreply.github.com>
2024-03-24 12:04:20 +0900

    Speed Buttons

--- LegoController/LegoController/ContentView.swift
+++ LegoController/LegoController/ContentView.swift
@@ -4,9 +4,11 @@ import LEGOKit
 struct ContentView: View {
     @StateObject var hub = Hub()
     @State var hexString = "08 00 81 00 11 51 00 32"
-    
+
+    @State var speed: Float = 0.0
     var body: some View {
         VStack {
+            Text("Direction")
             HStack{
                 Button{
 
@@ -42,6 +44,62 @@ struct ContentView: View {
                         .clipShape(Circle())
                 }
             }
+
+            Text("Speed")
+            HStack{
+                Button{
+                    speed = 0
+                }label: {
+                    Label("Stop", systemImage: "exclamationmark.octagon.fill")
+                        .frame(maxHeight: .infinity)
+                        .padding()
+                        .background(.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }
+                Button{
+                    speed = 0.25
+                }label: {
+                    Label("25% Speed", systemImage: "gauge.with.dots.needle.33percent")
+                        .frame(maxHeight: .infinity)
+                        .padding()
+                        .background(.green)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }.frame(maxHeight: .infinity)
+                Button{
+                    speed = 0.5
+                }label: {
+                    Label("50% Speed", systemImage: "gauge.with.dots.needle.50percent")
+                        .frame(maxHeight: .infinity)
+                        .padding()
+                        .background(.yellow)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }.frame(maxHeight: .infinity)
+                Button{
+                    speed = 0.75
+                }label: {
+                    Label("75% Speed", systemImage: "gauge.with.dots.needle.67percent")
+                        .frame(maxHeight: .infinity)
+                        .padding()
+                        .background(.orange)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }.frame(maxHeight: .infinity)
+                Button{
+                    speed = 1
+                }label: {
+                    Label("100% Speed", systemImage: "gauge.with.dots.needle.100percent")
+                        .frame(maxHeight: .infinity)
+                        .padding()
+                        .background(.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                }.frame(maxHeight: .infinity)
+
+            }.font(.title)
+                .foregroundStyle(.white)
+                .labelStyle(.iconOnly)
+                .fixedSize(horizontal: false, vertical: true)
+
+            Divider()
+                .padding()
             HStack {
                 if hub.isReady {
                     Text(hub.connectingPeripheral?.name ?? "No name")
@@ -62,8 +120,8 @@ struct ContentView: View {
                         hub.connect()
                     }
                 }
-            }
-            
+            }.buttonStyle(.bordered)
+
             HStack {
                 TextField("Hex Data", text: $hexString)
                     .padding(4)
